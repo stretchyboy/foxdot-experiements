78677690eff11b7e0148ca90c0c215628623113f
Martyn Eggleton <martyn.eggleton@gmail.com>
2018-12-02 11:15:54 +0000

    rearranging code for clarity

--- foxdot-rockstar-example.py
+++ foxdot-rockstar-example.py
@@ -1,7 +1,7 @@
 from foxdotrockstar import *
 
 #Transpile the following rockstar code into python and run it
-#code runs between the '''s 
+#code runs between the '''s
 #in the rockstar code ()s are comments
 rsf("tommy.rock", locals(),"tommies")
 
@@ -11,7 +11,7 @@ Papa was a rolling stone (Papa = 175)
 (loops down from 16)
 Tommy was a docker (Tommy = 16)
 While Tommy aint 0, (should be nothing, but there is a bug )
-Scream Tommy 
+Scream Tommy
 Knock Tommy down (Tommy --)
 
 (Finds the remainder of first num divided by second)
@@ -28,17 +28,19 @@ Give back my hope with my strength
 ''', locals(),"tommies") # brutal bit of python to get the variables and functions out of the Rockstar
 #the "tommies" takes alls the whispers and shouts etc. and puts them into an array called tommies
 
-Clock.bpm = Midnight(Papa, 200) # sets bpm to 175%200  
+Clock.bpm = Midnight(Papa, 200) # sets bpm to 175%200
 
 # Another brutal bit of python to use Lovin in an .every on a Player object
 @PlayerMethod
 def loving(self):
     global Clock
-    Clock.bpm = Lovin(Clock.bpm, 2)  
+    Clock.bpm = Lovin(Clock.bpm, 2)
 
 
 p1 >> pluck([0,4]).every(3, "loving")
 
-d1 >> play("x-o-").every(Pvar(tommies), "stutter")
 # And cancel it with
 #p1.never("loving")
+
+#using the array of tommies (the Shouted values)
+d1 >> play("x-o-").every(Pvar(tommies), "stutter")
